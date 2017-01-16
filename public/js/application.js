fbcc2f017fc213f524204e3a3d5c6eef020ab528
Peter Browse <peterbrowse@gmail.com>
2017-01-16 19:51:27 +0100

    Making some changes.

--- public/js/application.js
+++ public/js/application.js
@@ -1,11 +1,44 @@
 var debug = false;
 
 $(document).ready(function(){
-	var myLenticular = new Lenticular.Image($('.lenticular_container')[0], {
-		images: 'imgs/Test##.jpg',
-		frames: 24
+	set_size(function(){
+		var myLenticular = new Lenticular.Image($('.lenticular_container')[0], {
+			images: 'imgs/Test##.jpg',
+			frames: 24
+		});
 	});
 });
+
+function set_size(callback) {
+	window.scroll(0, 1);
+	
+	window.setTimeout(function() {
+		$(window).scrollTop(0); 
+	}, 0);
+	
+	$('.lenticular_container').css('width', $(window).width());
+	$('.lenticular_container').css('height', $(window).height());
+	
+	callback();
+}
+
+function preventScroll(e) {
+	e.preventDefault();
+}
+
+function setMouseCoords(e) {
+	mouseX = e.pageX;
+	mouseY = e.pageY;
+}
+
+function updateOrientation(e) {
+	if(window.orientation == 0) {
+		$body.removeClass('is-landscape').addClass('is-portrait');
+	} else {
+		$body.removeClass('is-portrait').addClass('is-landscape');
+	}
+	$(window).scrollTop(0).scrollLeft(0);
+}
 
 //FIXING FOREACH IN IE8
 if (typeof Array.prototype.forEach != 'function') {
